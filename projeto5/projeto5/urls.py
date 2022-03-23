b77a173bdf16fff9b410705a4f112fff7f895c71
A2LF <andreilucasa2@gmail.com>
2022-03-22 21:16:38 -0300

    Alteração de usuario

--- projeto5/projeto5/urls.py
+++ projeto5/projeto5/urls.py
@@ -20,7 +20,7 @@ from projeto5_website import views
 urlpatterns = [
     # Minha primeira view
     path('', views.home, name='home'),
-    path('admin/', admin.site.urls),
+    path('1admin/', admin.site.urls),
     path('pergunta_form/', views.pergunta_form, name='pergunta_form'),
     path('teste/<int:teste>/', views.teste, name='teste'),
 
